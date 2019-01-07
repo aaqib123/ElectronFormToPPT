2c439a13846e87e8f2d62290553c3874882adfde
Aaqib <aaqib.hyd@gmail.com>
2019-01-07 16:25:01 -0400

    multiple answer and titles included. Templating fixed,

--- main.js
+++ main.js
@@ -23,28 +23,117 @@ ipcMain.on("formdata", function(e, data) {
 
 createPPT = data => {
   console.log("ppt");
+  var questions = [
+    "Lorem ipsum dolor sit amet, iudico doming cum an, nemore posidonium constituam cu vis. Mea ei hinc nemore?",
+    "Lorem ipsum dolor sit amet, iudico doming cum an, nemore posidonium constituam cu vis. Mea ei hinc nemore?",
+    "Lorem ipsum dolor sit amet, iudico doming cum an, nemore posidonium constituam cu vis. Mea ei hinc nemore?",
+    "Lorem ipsum dolor sit amet, iudico doming cum an, nemore posidonium constituam cu vis. Mea ei hinc nemore?",
+    "Lorem ipsum dolor sit amet, iudico doming cum an, nemore posidonium constituam cu vis. Mea ei hinc nemore?"
+  ];
   var pptx = new pptxgenjs();
-  pptx.defineSlideMaster({
-    title: "MASTER_SLIDE",
-    bkgd: "FFFFFF",
-    objects: [
-      { line: { x: 3.5, y: 1.0, w: 6.0, line: "0088CC", lineSize: 5 } },
-      { rect: { x: 0.0, y: 5.3, w: "100%", h: 0.75, fill: "F1F1F1" } },
-      {
-        text: {
-          text: "Status Report",
-          options: { x: 3.0, y: 5.3, w: 5.5, h: 0.75 }
-        }
-      },
-      { image: { x: 11.3, y: 6.4, w: 1.67, h: 0.75, path: "images/logo.png" } }
-    ],
-    slideNumber: { x: 0.3, y: "90%" }
-  });
+
   var slide = pptx.addNewSlide();
-
-  var opts = { x: 1.0, y: 1.0, fontSize: 42, color: "00FF00" };
-
-  slide.addText(data.answer1, opts);
+  //Title
+  slide.addText(data.project, {
+    x: 0.2,
+    y: 0.0,
+    w: "98%",
+    valign: "top",
+    fontSize: 12,
+    bold: "true",
+    color: "000000"
+  });
+  //Question and Answers
+  slide.addText(questions[0], {
+    x: 0.2,
+    y: 0.5,
+    w: "48%",
+    valign: "top",
+    fontSize: 8,
+    autoFit: "true",
+    color: "000000"
+  });
+  slide.addText(data.answer1, {
+    x: 0.2,
+    y: 0.8,
+    w: "48%",
+    valign: "top",
+    fontSize: 8,
+    autoFit: "true",
+    color: "000000"
+  });
+  slide.addText(questions[1], {
+    x: 0.2,
+    y: 2.0,
+    w: "48%",
+    valign: "top",
+    fontSize: 8,
+    autoFit: "true",
+    color: "000000"
+  });
+  slide.addText(data.answer2, {
+    x: 0.2,
+    y: 2.3,
+    w: "48%",
+    valign: "top",
+    fontSize: 8,
+    autoFit: "true",
+    color: "000000"
+  });
+  slide.addText(questions[2], {
+    x: 0.2,
+    y: 3.0,
+    w: "48%",
+    valign: "top",
+    fontSize: 8,
+    autoFit: "true",
+    color: "000000"
+  });
+  slide.addText(data.answer3, {
+    x: 0.2,
+    y: 3.3,
+    w: "48%",
+    valign: "top",
+    fontSize: 8,
+    autoFit: "true",
+    color: "000000"
+  });
+  slide.addText(questions[3], {
+    x: 5.0,
+    y: 0.5,
+    w: "48%",
+    valign: "top",
+    fontSize: 8,
+    autoFit: "true",
+    color: "000000"
+  });
+  slide.addText(data.answer4, {
+    x: 5.0,
+    y: 0.8,
+    w: "48%",
+    valign: "top",
+    fontSize: 8,
+    autoFit: "true",
+    color: "000000"
+  });
+  slide.addText(questions[4], {
+    x: 5.0,
+    y: 3.0,
+    w: "48%",
+    valign: "top",
+    fontSize: 8,
+    autoFit: "true",
+    color: "000000"
+  });
+  slide.addText(data.answer5, {
+    x: 5.0,
+    y: 3.3,
+    w: "48%",
+    valign: "top",
+    fontSize: 8,
+    autoFit: "true",
+    color: "000000"
+  });
   pptx.save("CaseStudy", function(filename) {
     console.log("Created: " + filename);
   });
