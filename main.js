41516d51ec47dc142841c15d5168cbe97c3edd6e
Aaqib <aaqib.hyd@gmail.com>
2019-01-09 16:09:52 -0400

    ppt templating added and GUI css update

--- main.js
+++ main.js
@@ -1,11 +1,14 @@
 var pptxgenjs = require("pptxgenjs");
-const { app, BrowserWindow, ipcMain } = require("electron");
+const { app, BrowserWindow, ipcMain, dialog } = require("electron");
 
 app.on("ready", createWindow);
 
 function createWindow() {
   // Create the browser window.
-  win = new BrowserWindow({ width: 800, height: 600 });
+  win = new BrowserWindow({
+    width: 800,
+    height: 600
+  });
 
   // and load the index.html of the app.
   win.loadFile("index.html");
@@ -21,7 +24,18 @@ ipcMain.on("formdata", function(e, data) {
   createPPT(data);
 });
 
+/* data = {
+  project: "Title",
+  answer0: "this is answer 1",
+  answer1: "this is answer 2",
+  answer2: "this is answer 3",
+  answer3: "this is answer 4",
+  answer4: "this is answer 5"
+};
+createPPT(data); */
+
 createPPT = data => {
+  //function createPPT(data) {
   console.log("ppt");
   var questions = [
     "Lorem ipsum dolor sit amet, iudico doming cum an, nemore posidonium constituam cu vis. Mea ei hinc nemore?",
@@ -31,17 +45,49 @@ createPPT = data => {
     "Lorem ipsum dolor sit amet, iudico doming cum an, nemore posidonium constituam cu vis. Mea ei hinc nemore?"
   ];
   var pptx = new pptxgenjs();
+  //---------------------------------------------------------------------------------------------------
+  //Template design
+  pptx.defineSlideMaster({
+    title: "MASTER_SLIDE",
+    bkgd: "dcdce8",
+    objects: [
+      {
+        line: {
+          x: 5,
+          y: 0.8,
+          w: 0,
+          h: 3.5,
+          line: "0303bf",
+          lineSize: 1
+        }
+      },
+      {
+        rect: {
+          x: 0.1,
+          y: 0.1,
+          w: 9.8,
+          h: "96%",
+          line: "0303bf",
+          lineSize: 1
+        }
+      }
+      //    { image: { x: 6, y: 0, w: 1.67, h: 0.75, path: "images/logo.png" } }
+    ]
+  });
 
-  var slide = pptx.addNewSlide();
+  var slide = pptx.addNewSlide("MASTER_SLIDE");
+  //template design ends
+  //---------------------------------------------------------------------------------------------------
+  //var slide = pptx.addNewSlide();
   //Title
   slide.addText(data.project, {
     x: 0.2,
-    y: 0.0,
+    y: 0.1,
     w: "98%",
     valign: "top",
-    fontSize: 12,
+    fontSize: 14,
     bold: "true",
-    color: "000000"
+    color: "0303bf"
   });
   //Question and Answers
   slide.addText(questions[0], {
@@ -51,9 +97,9 @@ createPPT = data => {
     valign: "top",
     fontSize: 8,
     autoFit: "true",
-    color: "000000"
+    color: "0303bf"
   });
-  slide.addText(data.answer1, {
+  slide.addText(data.answer0, {
     x: 0.2,
     y: 0.8,
     w: "48%",
@@ -69,9 +115,9 @@ createPPT = data => {
     valign: "top",
     fontSize: 8,
     autoFit: "true",
-    color: "000000"
+    color: "0303bf"
   });
-  slide.addText(data.answer2, {
+  slide.addText(data.answer1, {
     x: 0.2,
     y: 2.3,
     w: "48%",
@@ -82,16 +128,16 @@ createPPT = data => {
   });
   slide.addText(questions[2], {
     x: 0.2,
-    y: 3.0,
+    y: 3.3,
     w: "48%",
     valign: "top",
     fontSize: 8,
     autoFit: "true",
-    color: "000000"
+    color: "0303bf"
   });
-  slide.addText(data.answer3, {
+  slide.addText(data.answer2, {
     x: 0.2,
-    y: 3.3,
+    y: 3.6,
     w: "48%",
     valign: "top",
     fontSize: 8,
@@ -105,9 +151,9 @@ createPPT = data => {
     valign: "top",
     fontSize: 8,
     autoFit: "true",
-    color: "000000"
+    color: "0303bf"
   });
-  slide.addText(data.answer4, {
+  slide.addText(data.answer3, {
     x: 5.0,
     y: 0.8,
     w: "48%",
@@ -123,9 +169,9 @@ createPPT = data => {
     valign: "top",
     fontSize: 8,
     autoFit: "true",
-    color: "000000"
+    color: "0303bf"
   });
-  slide.addText(data.answer5, {
+  slide.addText(data.answer4, {
     x: 5.0,
     y: 3.3,
     w: "48%",
@@ -134,7 +180,13 @@ createPPT = data => {
     autoFit: "true",
     color: "000000"
   });
-  pptx.save("CaseStudy", function(filename) {
+  pptx.save("CaseStudy-" + data.project, function(filename) {
     console.log("Created: " + filename);
+    dialog.showMessageBox(win, {
+      title: "Message",
+      buttons: [],
+      message: "PPT created with filename " + filename
+    });
+    return;
   });
 };
